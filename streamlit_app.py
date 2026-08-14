e288c0e2cdef09a9f65c5ca65f509c26978d35f5
PoojaKOjha <pooja.k.ojha@fa-techaustralia.com.au>
2026-08-14 22:35:31 +0800

    Update streamlit_app.py
    watermelon change

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+    
     my_insert_stmt = """ insert into smoothies.public.orders (name_on_order, ingredients)
         values ('""" + name_on_order + """', '""" + ingredients_string + """') """
     st.stop()
@@ -63,5 +66,4 @@ else:
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
-#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
