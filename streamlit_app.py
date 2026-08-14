e894b0f72d83596d4ccd79a6e76a4ee9292b28be
PoojaKOjha <pooja.k.ojha@fa-techaustralia.com.au>
2026-08-14 15:56:12 +0800

    Add API call to fetch watermelon data
    Fetch and display watermelon data from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,3 +60,7 @@ if my_dataframe:
 else:
     st.success("There are no pending orders right now.", icon="👍")
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
